73847739d473997f04989e68f9fb975f91914f49
grinikita <Grigoriev98>
2019-11-02 10:10:15 +0300

    test 5

--- src/stringCalc.js
+++ src/stringCalc.js
@@ -1,8 +1,14 @@
 
-const calc = (numbers) => {
-  const numbers_splited = numbers.split(/[,\n]/)
-  return numbers_splited.reduce((accum, cur) => accum + Number(cur), 0)
+const calc = (input) => {
+  let numbers = input;
+  let delimiter = new RegExp('[,\n]');
+  if (numbers.slice(0, 2) === '//') {
+    const customDelimiter = numbers.split('\n')[0].slice(2);
+    delimiter = new RegExp(customDelimiter + '|' + '[\n]', 'g')
+    numbers = input.split('\n', 2)[1];
+  }
+  const numbers_splited = numbers.split(delimiter);
+  return numbers_splited.reduce((accum, cur) => accum + Number(cur), 0);
 }
-
 
 export default calc
