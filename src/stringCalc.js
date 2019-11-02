d59a233f185df09cb4d034ac577a79140ea64e04
cepuchino <niyaz1998@yandex.ru>
2019-11-02 09:33:38 +0300

    cleared second wave

--- src/stringCalc.js
+++ src/stringCalc.js
@@ -1,4 +1,15 @@
 
-const calc = () => 0
+const calc = (str) => {
+    if (typeof str !== 'undefined') {
+        let vars = str.split(',');
+        let res = 0;
+        vars.forEach((v) => {
+            res += Number(v)
+        });
+        return res;
+    } else {
+        return 0;
+    }
+};
 
 export default calc
