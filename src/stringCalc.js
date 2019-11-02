3afe6ca5bb95ff53e40ce57baf7acae1ad5fa381
nariman9119 <nariman9119@gmail.com>
2019-11-02 09:56:28 +0300

    4 wave

--- src/stringCalc.js
+++ src/stringCalc.js
@@ -1,15 +1,14 @@
-
-const stringCalc = (text) =>{
-    if (text===''){
+const stringCalc = (text) => {
+    if (text === '') {
         return 0
     }
-    if (text.split(',').length===1){
+    if (text.split(',').length === 1 && text.indexOf('\n') !== -1) {
         return 1
-    }
-
-    if (text.split(',').length>1){
+    } else {
         var total = 0;
-        for (var item in text.split(',')){
+        text = text.replace('\n', ',')
+        console.log(text)
+        for (var item in text.split(',')) {
             total = total + parseInt(text.split(',')[item])
 
         }
