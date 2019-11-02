d5410f263a3128a39cf1521e4054a7209aec660f
Alexey Evlampev <a.evlampev@innopolis.ru>
2019-11-02 10:13:42 +0300

    6 wave

--- src/stringCalc.js
+++ src/stringCalc.js
@@ -8,6 +8,9 @@ const calc = (str) => {
         str = matches[2];
     }
     const splitted = str.replace('\n',',').split(delimiter);
+    if (splitted.some(val => val < 0)) {
+        throw new Error("negatives not allowed");
+    }
     return splitted.reduce((acc, val) => acc + parseInt(val), 0);
 };
 
