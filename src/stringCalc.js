3fd954e90f58b7ceaeaeebc159b72c61235c33fd
grinikita <Grigoriev98>
2019-11-02 10:18:12 +0300

    Anable answers to be merged

--- src/stringCalc.js
+++ src/stringCalc.js
@@ -1,23 +1,4 @@
-const parseNumber = (number) => {
-  const num = Number(number);
-  if (num < 0) {
-    throw new Error('negatives not allowed')
-  } else if (num > 1000) {
-    return 0
-  }
-  return num;
-}
 
-const calc = (input) => {
-  let numbers = input;
-  let delimiter = new RegExp('[,\n]');
-  if (numbers.slice(0, 2) === '//') {
-    const customDelimiter = numbers.split('\n')[0].slice(2);
-    delimiter = new RegExp(customDelimiter + '|' + '[\n]', 'g')
-    numbers = input.split('\n', 2)[1];
-  }
-  const numbers_splited = numbers.split(delimiter);
-  return numbers_splited.reduce((accum, cur) => accum + parseNumber(cur), 0);
-}
+const calc = () => 0
 
 export default calc
