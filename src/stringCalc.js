d94414b6a81f7a0749c93e07a9c450179ed2ef6f
tarexme <hello@tarex.me>
2019-11-02 09:38:59 +0300

    asd

--- src/stringCalc.js
+++ src/stringCalc.js
@@ -1,4 +1,19 @@
 
-const calc = () => 0
+const calc = (arg) => {
+    let nums = arg.split(',');
+    let result = 0;
+
+    console.log(result);
+
+    nums.forEach(function(element) {
+        let currentNum = parseInt(element);
+
+        if (!isNaN(currentNum)) {
+            result += currentNum;
+        }
+    });
+
+    return result;
+}
 
 export default calc
