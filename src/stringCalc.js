4afb845b506bb1ffe4afe15f6f5bbd4b53573a75
cepuchino <niyaz1998@yandex.ru>
2019-11-02 10:20:01 +0300

    calc to default

--- src/stringCalc.js
+++ src/stringCalc.js
@@ -1,23 +1,4 @@
-const calc = (str) => {
-    if (typeof str !== 'undefined') {
-        let vars = str.split(new RegExp('[\\n,/;p]'));
-        console.log(vars);
-        let res = 0;
-        vars.forEach((v) => {
-            if (v !== '') {
-                if (v < 0) {
-                    throw Error('negatives not allowed');
-                } else if (v > 1000) {
 
-                } else {
-                    res += Number(v)
-                }
-            }
-        });
-        return res;
-    } else {
-        return 0;
-    }
-};
+const calc = () => 0
 
 export default calc
