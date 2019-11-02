841cbbfe734ac5af47604d42eac715cfdfe18cd4
Alexey Evlampev <a.evlampev@innopolis.ru>
2019-11-02 09:28:33 +0300

    1 wave

--- src/stringCalc.js
+++ src/stringCalc.js
@@ -1,4 +1,11 @@
 
-const calc = () => 0
+const calc = (str) => {
+    if (str === '') return 0;
+    const splitted = str.split(',');
+    if (splitted.length === 1) {
+        return parseInt(splitted[0]);
+    }
+    return parseInt(splitted[0]) + parseInt(splitted[1]);
+};
 
 export default calc
