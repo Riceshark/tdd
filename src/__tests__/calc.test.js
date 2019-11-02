e3e841fe59b1f6b35da72dd86436c60ea45b6fd1
Александр Примаков <primakovpro@gmail.com>
2019-11-02 09:25:29 +0300

    second wave

--- src/__tests__/calc.test.js
+++ src/__tests__/calc.test.js
@@ -3,5 +3,15 @@ import stringCalc from '../stringCalc';
 describe('first tests wave', () => {
     it('do some simple stuff', () => {
         expect(stringCalc('')).toBe(0);
-    })
+    });
+})
+
+describe('second tests wave', () => {
+    it('adds numbers', () => {
+        expect(stringCalc('1')).toBe(1);
+    });
+
+    it('adds numbers', () => {
+        expect(stringCalc('1,2')).toBe(3);
+    });
 })
