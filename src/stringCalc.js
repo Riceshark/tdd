137583859abecafebe62fcb65d0873372ed73cca
Alexey Evlampev <a.evlampev@innopolis.ru>
2019-11-02 09:50:19 +0300

    3 wave

--- src/stringCalc.js
+++ src/stringCalc.js
@@ -1,7 +1,7 @@
 
 const calc = (str) => {
     if (str === '') return 0;
-    const splitted = str.split(',');
+    const splitted = str.replace('\n',',').split(',');
     return splitted.reduce((acc, val) => acc + parseInt(val), 0);
 };
 
