76b7faae574ac3804382ee607875a2cc8b71adcc
Riceshark <enotpetya@gmail.com>
2019-11-05 23:14:51 +0300

    Finished third wave

--- src/__tests__/calc.test.js
+++ src/__tests__/calc.test.js
@@ -15,16 +15,16 @@ describe('second tests wave', () => {
         expect(stringCalc('1,2')).toBe(3);
     });
 })
-//
-// describe('3 tests wave', () => {
-//     it('adds several numbers', () => {
-//         expect(stringCalc('1,2,3,4,5')).toBe(15);
-//     });
-//
-//     it('adds several numbers', () => {
-//         expect(stringCalc('1,2,2,3')).toBe(8);
-//     });
-// })
+
+describe('3 tests wave', () => {
+    it('adds several numbers', () => {
+        expect(stringCalc('1,2,3,4,5')).toBe(15);
+    });
+
+    it('adds several numbers', () => {
+        expect(stringCalc('1,2,2,3')).toBe(8);
+    });
+})
 //
 // describe('4 tests wave', () => {
 //     it('handles new line', () => {
@@ -35,7 +35,7 @@ describe('second tests wave', () => {
 //         expect(stringCalc('1,2\n3,1')).toBe(7);
 //     });
 // })
-//
+
 // describe('5 handle castom delimiter like "//[delimiter]\n[numbers...]"', () => {
 //     it('handle ; as delimeter', () => {
 //         expect(stringCalc('//;\n1;2')).toBe(3);
--- src/stringCalc.js
+++ src/stringCalc.js
@@ -1,12 +1,16 @@
 const calc = (input) => {
     const values = input.split(',');
-    console.log(values)
-    let result = 0
-    values.forEach((item) =>{
-        result += Number(item);
+    console.log(values);
+    let result = 0;
 
-    })
+    values.forEach((item) =>{
+        result += parseInt(item);
+
+    });
+    if (input === '' || undefined){
+        result = 0;
+    }
     return result
-}
+};
 
 export default calc
