031c04e862182085d99090b3243eec7ad67b1df9
Ramil Akhmetshin <r.akhmetshin@bars-open.ru>
2019-11-08 23:54:33 +0300

    test 5th wave

--- src/stringCalc.js
+++ src/stringCalc.js
@@ -1,11 +1,18 @@
 
 const calc = (arg) => {
-  const numbers = arg.split(/[,\n]/);
+  const start = arg.slice(0,2);
+  let numbers = []
+  if (start === "//") {
+    const delimeter = arg[2];
+    numbers = arg.slice(4, arg.length).split(delimeter);
+  } else {
+    numbers = arg.split(/[,\n]/);
+  }
   if (numbers.length === 1 && numbers[0] === '')
     return 0;
   let sum = 0;
   numbers.forEach(x => sum += Number(x));
   return sum;
-}
+};
 
 export default calc
