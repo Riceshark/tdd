28672790614bd5919706bb3595b79a79a20a8035
Ramil Akhmetshin <r.akhmetshin@bars-open.ru>
2019-11-09 00:06:00 +0300

    test 6th wave

--- src/stringCalc.js
+++ src/stringCalc.js
@@ -1,18 +1,21 @@
-
 const calc = (arg) => {
-  const start = arg.slice(0,2);
-  let numbers = []
-  if (start === "//") {
-    const delimeter = arg[2];
-    numbers = arg.slice(4, arg.length).split(delimeter);
-  } else {
-    numbers = arg.split(/[,\n]/);
-  }
-  if (numbers.length === 1 && numbers[0] === '')
-    return 0;
-  let sum = 0;
-  numbers.forEach(x => sum += Number(x));
-  return sum;
+    const start = arg.slice(0, 2);
+    let numbers = []
+    if (start === "//") {
+        const delimeter = arg[2];
+        numbers = arg.slice(4, arg.length).split(delimeter);
+    } else {
+        numbers = arg.split(/[,\n]/);
+    }
+    if (numbers.length === 1 && numbers[0] === '')
+        return 0;
+    let sum = 0;
+    numbers.forEach(x => {
+        if (Number(x) < 0) {
+            throw new Error('negatives not allowed');
+        } else sum += Number(x)
+    });
+    return sum;
 };
 
 export default calc
