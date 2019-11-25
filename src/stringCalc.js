a0fb27d99f4844bee51bf0bbc4a19b69ddd32ac3
Riceshark <enotpetya@gmail.com>
2019-11-25 22:19:47 +0300

    Update stringCalc.js

--- src/stringCalc.js
+++ src/stringCalc.js
@@ -1,24 +1,3 @@
-const calc = (input) => {
-    input = input.toString();
-    let regex = /[+-]?\d+(?:\.\d+)?/g;
-    let result = 0;
-
-    let match = regex.exec(input);
-    while (match) {
-        let number = parseInt(match[0]);
-        if (number < 1000) {
-            if (number < 0) {
-                throw new Error("negatives not allowed");
-            }
-            result += number;
-        }
-        match = regex.exec(input)
-    }
-    if (input === '' || undefined) {
-        result = 0;
-    }
-
-    return result
-};
+const calc = () => 0
 
 export default calc
